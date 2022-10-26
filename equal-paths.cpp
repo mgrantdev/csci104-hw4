706ef3e62eaf3f476619860a38eec8e8ae71043f
Matthew Grant <mcgrant@usc.edu>
2022-10-26 05:17:36 +0000

    Added equal path checks

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,4 +1,5 @@
 #include "equal-paths.h"
+#include <iostream>
 using namespace std;
 
 
@@ -9,15 +10,33 @@ bool equalPaths(Node * root)
 {
     // Add your code below
 
-    // if root is empty, return false (singular node can't be equal)
-    if(root == NULL) return false;
+    // if root is empty, return true(singular node can't be equal)
+    if(root == NULL) return true;
 
     // if left and right paths are equal, return true
-    bool leftEqual = equalPaths(root->left);
-    bool rightEqual = equalPaths(root->right);
+    int leftEqual = traversePath(root->left);
+    int rightEqual = traversePath(root->right);
     if(leftEqual == rightEqual) return true;
 
     // otherwise, return false
     return false;
 }
 
+int traversePath(Node * n) {
+
+	// if node is empty, return 0 (no paths)
+	if(n == NULL) return 0;
+
+	// if left path exists, add to tally
+	int pLength = 0;
+	if(n->left != NULL) {
+		pLength = 2 + traversePath(n->left);
+	}
+
+	// if right path exists, add to tally
+	if(n->right != NULL) {
+		pLength = 2 + traversePath(n->right);
+	}
+	return pLength;
+
+}
