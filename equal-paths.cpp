9043f6f0f68c3105c0beceb91214abdeea83abf8
Matthew Grant <mgrant.xi@gmail.com>
2022-10-25 20:30:31 -0700

    added equal path check

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,5 +9,15 @@ bool equalPaths(Node * root)
 {
     // Add your code below
 
+    // if root is empty, return false (singular node can't be equal)
+    if(root == NULL) return false;
+
+    // if left and right paths are equal, return true
+    bool leftEqual = equalPaths(root->left);
+    bool rightEqual = equalPaths(root->right);
+    if(leftEqual == rightEqual) return true;
+
+    // otherwise, return false
+    return false;
 }
 
